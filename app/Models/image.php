65c7894b5d320e43aed7a56467f60137a8879b3a
cmunhozprog <cmunhozprog03@hotmail.com>
2020-12-29 17:59:24 -0300

    Migrations e Seeders

--- app/Models/image.php
+++ app/Models/image.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class image extends Model
+class Image extends Model
 {
     use HasFactory;
-
+    
     protected $guarded = ['id'];
 
     //Relação poliformica
